bcc441f12462fe798c3de78d53ca831f6e91627a
Rick-Krijger <9019173@student.zadkine.nl>
2022-09-15 15:04:40 +0200

    oefening 1

---
+++ oefening 1/app.js
@@ -0,0 +1,28 @@
+const myName = "Rick";
+let myAge = 18;
+const diploma = true;
+let passed;
+
+document.write(myName);
+document.write(myAge);
+document.write(diploma);
+document.write(passed);
+
+
+let num1 = 35;
+let num2 = 22;
+
+let answer = num1 + num2;
+document.write(answer);
+
+let answer2 = num1 - num2;
+document.write(answer2);
+
+let answer3 = num1 * num2;
+document.write(answer3);
+
+let answer4 = num1 / num2;
+document.write(answer4);
+
+let answer5 = num1 % num2;
+document.write(answer5);
