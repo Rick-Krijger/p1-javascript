49db9f5e17f525bfaaf5040c9f38babe24dd6d0b
Rick-Krijger <9019173@student.zadkine.nl>
2022-09-22 17:58:15 +0200

    opdracht 2 denk ik

---
+++ test-ofzo/app.js
@@ -0,0 +1,25 @@
+function color_change() {
+    let title = "title";
+
+    document.getElementById(title).style.color = "red";
+    document.getElementById(title).style.fontWeight = "normal";
+    document.getElementById(title).style.fontSize = "3rem";
+}
+
+
+function color_change_back() {
+    let title = "title";
+
+    document.getElementById(title).style.color = "black";
+}
+
+function background_color_change() {
+    let paragraph = "paragraph";
+
+    document.getElementById(paragraph).style.backgroundColor = "red";
+}
+
+function change_text() {
+    let text = document.getElementById('text');
+    text.innerText = 'Tekst weg!';
+}
