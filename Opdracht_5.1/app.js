8e1586904d89896d4022dcb9df3d96dd02513391
Rick-Krijger <9019173@student.zadkine.nl>
2022-10-04 13:12:06 +0200

    Opdracht 5.1

---
+++ Opdracht_5.1/app.js
@@ -0,0 +1,10 @@
+function text_change() {
+    let text = document.getElementById('title');
+    text.innerText = 'Ik ben nog steeds een titel!';
+}
+
+function color_change() {
+    let title = document.getElementById("title");
+
+    title.style.color = "red";
+}
