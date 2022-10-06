93599af52fa9862b19606b5430ee8663688671e5
Rick-Krijger <9019173@student.zadkine.nl>
2022-10-06 14:47:49 +0200

    opdracht_6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,26 @@
+// Oefening 1
+
+function calculate(num,num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+calculate(123,456)
+
+// Oefening 2
+
+function multiplyByFive(num3) {
+    let answer2 = num3 * 5;
+    console.log(answer2)
+}
+
+multiplyByFive(5)
+
+// Oefening 3
+
+function calculateMinutes(num4) {
+    let answer3 = num4 / 60
+    console.log(answer3)
+}
+
+calculateMinutes(120)
