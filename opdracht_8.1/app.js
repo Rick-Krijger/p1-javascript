2da3a56b2d559339f3e5cb59b9446b3ed5e32fcf
Rick-Krijger <9019173@student.zadkine.nl>
2022-11-01 11:35:06 +0100

    Opdracht 8.2 - Calculator uitbreiden

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -6,27 +6,43 @@ let answer
 function multiply() {
     inputone = document.getElementById("inputone").valueAsNumber;
     inputtwo = document.getElementById("inputtwo").valueAsNumber;
-    answer = inputone * inputtwo
-    text.innerText = "het antwoord is: " + answer;
+    if (inputone > 0 &&	inputtwo > 0) {
+        answer = inputone * inputtwo
+        text.innerText = "het antwoord is: " + answer;
+    } else {
+        text.innerText = "het getal is te laag";
+    }
 }
 
 function minus() {
     inputone = document.getElementById("inputone").valueAsNumber;
     inputtwo = document.getElementById("inputtwo").valueAsNumber;
-    answer = inputone - inputtwo
-    text.innerText = "het antwoord is: " + answer;
+    if (inputone > 0 &&	inputtwo > 0) {
+        answer = inputone - inputtwo
+        text.innerText = "het antwoord is: " + answer;
+    } else {
+        text.innerText = "het getal is te laag";
+    }
 }
 
 function add() {
     inputone = document.getElementById("inputone").valueAsNumber;
     inputtwo = document.getElementById("inputtwo").valueAsNumber;
-    answer = inputone + inputtwo
-    text.innerText = "het antwoord is: " + answer;
+    if (inputone > 0 &&	inputtwo > 0) {
+        answer = inputone + inputtwo
+        text.innerText = "het antwoord is: " + answer;
+    } else {
+        text.innerText = "het getal is te laag";
+    }
 }
 
 function devide() {
     inputone = document.getElementById("inputone").valueAsNumber;
     inputtwo = document.getElementById("inputtwo").valueAsNumber;
-    answer = inputone / inputtwo
-    text.innerText = "het antwoord is: " + answer;
+    if (inputone > 0 &&	inputtwo > 0) {
+        answer = inputone / inputtwo
+        text.innerText = "het antwoord is: " + answer;
+    } else {
+        text.innerText = "het getal is te laag";
+    }
 }
